113713a5a1442e67b13cd2f6c868b90239aa6742
beelleed <96171946+beelleed@users.noreply.github.com>
2025-05-07 19:27:05 +0800

    code : update solution for two sum

--- array/001_two_sum.py
+++ array/001_two_sum.py
@@ -4,3 +4,12 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return i,j
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}  # key: number, value: index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
