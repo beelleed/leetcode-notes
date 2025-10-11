3c3c3f59422c77b0bd69136ad3b3bf3e99cd5c7f
beelleed <96171946+beelleed@users.noreply.github.com>
2025-10-11 02:27:33 -0700

    add 283

---
+++ array & Hashmap/283_move_zeros.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        # 函式沒有 return，因為是原地修改 nums
